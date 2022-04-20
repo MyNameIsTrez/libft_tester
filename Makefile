ed44669555ec9f4608de9cd986f8bb7564eaad81
MyNameIsTrez <sbos@student.codam.nl>
2022-04-20 14:45:48 +0200

    Slim down by removing exclude_tests.

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: sbos <sbos@student.codam.nl>                 +#+                      #
 #                                                    +#+                       #
 #    Created: 2022/02/04 14:13:59 by sbos          #+#    #+#                  #
-#    Updated: 2022/04/20 14:20:27 by sbos          ########   odam.nl          #
+#    Updated: 2022/04/20 14:24:26 by sbos          ########   odam.nl          #
 #                                                                              #
 # **************************************************************************** #
 
@@ -36,9 +36,7 @@ TESTER_HEADERS :=												\
 	$(TESTS_DIR)/libft_tests.h									\
 	$(MASSERT_DIR)/massert.h									\
 	$(CTESTER_DIR)/libctester.h									\
-	$(CTESTER_DIR)/src/unstable/libft_unstable.h				\
 	$(CTESTER_DIR)/src/unstable/overwritten_headers/stdlib.h
-# TODO: ^ Can this header be removed from here? If not, add the others from CTESTER_DIR?
 
 MASSERT := $(MASSERT_DIR)/libmassert.a
 CTESTER := $(CTESTER_DIR)/libctester.a
--- tests/tester.c
+++ tests/tester.c
@@ -6,22 +6,13 @@
 /*   By: sbos <sbos@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/02/05 17:07:20 by sbos          #+#    #+#                 */
-/*   Updated: 2022/04/06 15:34:19 by sbos          ########   odam.nl         */
+/*   Updated: 2022/04/20 14:44:14 by sbos          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 ////////////////////////////////////////////////////////////////////////////////
 
-#include "libft.h"
-
-////////////////////////////////////////////////////////////////////////////////
-
 #include "libft_tests.h"
-
-////////////////////////////////////////////////////////////////////////////////
-
-# include <stdio.h>		// printf()
-# include <stdlib.h>	// EXIT_SUCCESS
 
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -58,29 +49,14 @@ t_list	*test_lst_new_front(t_list **lst, void *content)
 
 ////////////////////////////////////////////////////////////////////////////////
 
-static bool	str_in_arr(char *str, char *arr[], size_t sizeof_arr)
-{
-	size_t i = 0;
-	while (i < sizeof_arr / sizeof(char *))
-	{
-		if (ft_strcmp(arr[i], str) == 0)
-			return (true);
-		i++;
-	}
-	return (false);
-}
-
-static void	run_tests(char *exclude_tests[], size_t sizeof_exclude_tests)
+static void	run_tests(void)
 {
 	t_list	*lst = g_tests_lst;
 	while (lst != NULL)
 	{
 		t_fn_info *fn = lst->content;
-		if (NOT str_in_arr(fn->fn_name, exclude_tests, sizeof_exclude_tests))
-		{
-			printf("Testing function '%s'\n", fn->fn_name);
-			fn->fn_ptr();
-		}
+		printf("Testing function '%s'\n", fn->fn_name);
+		fn->fn_ptr();
 		lst = lst->next;
 	}
 }
@@ -91,16 +67,12 @@ int	main(void)
 {
 	printf("\nRunning tests...\n");
 
-	char *exclude_tests[] = {
-		// "ft_str_repeat",
-	};
-
 	malloc_call_count = 0;
 	write_call_count = 0;
 	malloc_call_count_to_fail = 0;
 	write_call_count_to_fail = 0;
 
-	run_tests(exclude_tests, sizeof(exclude_tests));
+	run_tests();
 
 	int max_i = ft_max(malloc_call_count, write_call_count);
 	int write_fail_offset = max_i / 2;
@@ -114,7 +86,7 @@ int	main(void)
 		malloc_call_count_to_fail = i;
 		write_call_count_to_fail = ((i + write_fail_offset) % max_i) + 1;
 
-		run_tests(exclude_tests, sizeof(exclude_tests));
+		run_tests();
 
 		i++;
 	}
