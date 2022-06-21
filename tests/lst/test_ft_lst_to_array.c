0615a5b3e9a20dcc815305d9a7a63a012cb8e9ec
MyNameIsTrez <sbos@student.codam.nl>
2022-06-21 12:27:08 +0200

    Add tests for ft_lst_to_array()

---
+++ tests/lst/test_ft_lst_to_array.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   test_ft_lst_to_array.c                             :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: sbos <sbos@student.codam.nl>                 +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/06/21 12:10:59 by sbos          #+#    #+#                 */
+/*   Updated: 2022/06/21 12:22:18 by sbos          ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+////////////////////////////////////////////////////////////////////////////////
+
+#include "libft_tests.h"
+
+////////////////////////////////////////////////////////////////////////////////
+
+Test(ft_lst_to_array)
+{
+	t_list	*lst = NULL;
+
+	ft_lst_new_front(&lst, "foo");
+	m_safe_assert(void *, lst, lst, NULL, true);
+
+	ft_lst_new_front(&lst, "bar");
+	m_safe_assert(void *, lst, lst, NULL, true);
+
+	void	**array = ft_lst_to_array(lst);
+	m_safe_assert(void *, array, array, NULL, true);
+
+	if (array != NULL)
+	{
+		m_safe_string_assert(array[0], "bar", true);
+		m_safe_string_assert(array[1], "foo", true);
+	}
+}
+
+////////////////////////////////////////////////////////////////////////////////
