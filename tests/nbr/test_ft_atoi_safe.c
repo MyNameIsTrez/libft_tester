5465caa34ea8a9b47018edfdceb2106938b4b3fb
MyNameIsTrez <sbos@student.codam.nl>
2022-06-23 12:56:49 +0200

    Updated libft and tests.

--- tests/nbr/test_ft_atoi_safe.c
+++ tests/nbr/test_ft_atoi_safe.c
@@ -6,7 +6,7 @@
 /*   By: sbos <sbos@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/02/16 14:30:21 by sbos          #+#    #+#                 */
-/*   Updated: 2022/04/06 15:34:19 by sbos          ########   odam.nl         */
+/*   Updated: 2022/06/23 12:56:10 by sbos          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,29 +21,29 @@ Test(ft_atoi_safe)
 	{
 		{
 			int	nbr = 420;
-			massert(ft_atoi_safe("-12345", &nbr), (bool)true);
+			massert(ft_atoi_safe("-12345", &nbr), (t_success)SUCCESS);
 			massert(nbr, -12345);
 		}
 
 		{
 			int	nbr = 420;
-			massert(ft_atoi_safe("+12345", &nbr), (bool)true);
+			massert(ft_atoi_safe("+12345", &nbr), (t_success)SUCCESS);
 			massert(nbr, 12345);
 		}
 
 		{
 			int	nbr = 420;
-			massert(ft_atoi_safe(".12345", &nbr), (bool)false);
+			massert(ft_atoi_safe(".12345", &nbr), (t_success)ERROR);
 		}
 
 		{
 			int	nbr = 420;
-			massert(ft_atoi_safe("a12345", &nbr), (bool)false);
+			massert(ft_atoi_safe("a12345", &nbr), (t_success)ERROR);
 		}
 
 		{
 			int	nbr = 420;
-			massert(ft_atoi_safe(" 12345 ", &nbr), (bool)true);
+			massert(ft_atoi_safe(" 12345 ", &nbr), (t_success)SUCCESS);
 			massert(nbr, 12345);
 		}
 	}
@@ -51,22 +51,22 @@ Test(ft_atoi_safe)
 	{
 		{
 			int	nbr = 420;
-			massert(ft_atoi_safe("- 12345", &nbr), (bool)false);
+			massert(ft_atoi_safe("- 12345", &nbr), (t_success)ERROR);
 		}
 
 		{
 			int	nbr = 420;
-			massert(ft_atoi_safe("+ 12345", &nbr), (bool)false);
+			massert(ft_atoi_safe("+ 12345", &nbr), (t_success)ERROR);
 		}
 
 		{
 			int	nbr = 420;
-			massert(ft_atoi_safe(". 12345", &nbr), (bool)false);
+			massert(ft_atoi_safe(". 12345", &nbr), (t_success)ERROR);
 		}
 
 		{
 			int	nbr = 420;
-			massert(ft_atoi_safe("a 12345", &nbr), (bool)false);
+			massert(ft_atoi_safe("a 12345", &nbr), (t_success)ERROR);
 			massert(nbr, 420);
 		}
 	}
@@ -74,25 +74,25 @@ Test(ft_atoi_safe)
 	{
 		{
 			int	nbr = 420;
-			massert(ft_atoi_safe("123-45", &nbr), (bool)true);
+			massert(ft_atoi_safe("123-45", &nbr), (t_success)SUCCESS);
 			massert(nbr, 123);
 		}
 
 		{
 			int	nbr = 420;
-			massert(ft_atoi_safe("123+45", &nbr), (bool)true);
+			massert(ft_atoi_safe("123+45", &nbr), (t_success)SUCCESS);
 			massert(nbr, 123);
 		}
 
 		{
 			int	nbr = 420;
-			massert(ft_atoi_safe("123.45", &nbr), (bool)true);
+			massert(ft_atoi_safe("123.45", &nbr), (t_success)SUCCESS);
 			massert(nbr, 123);
 		}
 
 		{
 			int	nbr = 420;
-			massert(ft_atoi_safe("123a45", &nbr), (bool)true);
+			massert(ft_atoi_safe("123a45", &nbr), (t_success)SUCCESS);
 			massert(nbr, 123);
 		}
 	}
@@ -100,25 +100,25 @@ Test(ft_atoi_safe)
 	{
 		{
 			int	nbr = 420;
-			massert(ft_atoi_safe("12345-", &nbr), (bool)true);
+			massert(ft_atoi_safe("12345-", &nbr), (t_success)SUCCESS);
 			massert(nbr, 12345);
 		}
 
 		{
 			int	nbr = 420;
-			massert(ft_atoi_safe("12345+", &nbr), (bool)true);
+			massert(ft_atoi_safe("12345+", &nbr), (t_success)SUCCESS);
 			massert(nbr, 12345);
 		}
 
 		{
 			int	nbr = 420;
-			massert(ft_atoi_safe("12345.", &nbr), (bool)true);
+			massert(ft_atoi_safe("12345.", &nbr), (t_success)SUCCESS);
 			massert(nbr, 12345);
 		}
 
 		{
 			int	nbr = 420;
-			massert(ft_atoi_safe("12345a", &nbr), (bool)true);
+			massert(ft_atoi_safe("12345a", &nbr), (t_success)SUCCESS);
 			massert(nbr, 12345);
 		}
 	}
@@ -126,24 +126,24 @@ Test(ft_atoi_safe)
 	{
 		{
 			int	nbr = 420;
-			massert(ft_atoi_safe("2147483647", &nbr), (bool)true);
+			massert(ft_atoi_safe("2147483647", &nbr), (t_success)SUCCESS);
 			massert(nbr, 2147483647);
 		}
 
 		{
 			int	nbr = 420;
-			massert(ft_atoi_safe("2147483648", &nbr), (bool)false);
+			massert(ft_atoi_safe("2147483648", &nbr), (t_success)ERROR);
 		}
 
 		{
 			int	nbr = 420;
-			massert(ft_atoi_safe("-2147483648", &nbr), (bool)true);
+			massert(ft_atoi_safe("-2147483648", &nbr), (t_success)SUCCESS);
 			massert(nbr, (int)-2147483648);
 		}
 
 		{
 			int	nbr = 420;
-			massert(ft_atoi_safe("-2147483649", &nbr), (bool)false);
+			massert(ft_atoi_safe("-2147483649", &nbr), (t_success)ERROR);
 		}
 	}
 }
