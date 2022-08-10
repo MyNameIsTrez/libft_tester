c1b66384a5c2b2670fafa716a4133293b08a4a6e
MyNameIsTrez <sbos@student.codam.nl>
2022-08-10 13:01:59 +0200

    Add `ft_vector_swap_remove()` tests

--- tests/vector/test_ft_vector_swap_remove.c
+++ tests/vector/test_ft_vector_swap_remove.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   test_ft_vector.c                                   :+:    :+:            */
+/*   test_ft_vector_swap_remove.c                       :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: sbos <sbos@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/07/19 16:27:25 by sbos          #+#    #+#                 */
-/*   Updated: 2022/08/10 12:54:29 by sbos          ########   odam.nl         */
+/*   Updated: 2022/08/10 13:00:28 by sbos          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,28 @@
 
 ////////////////////////////////////////////////////////////////////////////////
 
-Test(ft_vector)
+// TODO: Remove
+t_metadata	*get_metadata_ptr(void *vector);
+
+Test(ft_vector_swap_remove_out_of_bounds)
 {
 	int	*ints;
-	int	v;
 
 	m_safe_assert(void *, ints = ft_vector_new(sizeof(int)), ints, NULL, false);
 
 	if (NOT was_malloc_unstable)
 	{
-		m_safe_assert(t_status, ft_vector_reserve(&ints, 1), OK, ERROR, false);
+		ft_vector_swap_remove(&ints, 0);
+		m_safe_assert(t_ft_error, ft_get_error(), FT_ERROR_OUT_OF_BOUNDS, FT_ERROR_MALLOC, false);
 	}
+}
+
+Test(ft_vector_swap_remove)
+{
+	int	*ints;
+	int	v;
+
+	m_safe_assert(void *, ints = ft_vector_new(sizeof(int)), ints, NULL, false);
 
 	int i = 0;
 	while (i < 1000)
@@ -42,9 +53,13 @@ Test(ft_vector)
 	}
 
 	if (NOT was_malloc_unstable)
-	{
-		massert(ft_vector_get_size(ints), (size_t)1000);
-	}
+		m_safe_assert(t_status, ft_vector_swap_remove(&ints, 0), OK, ERROR, false);
+
+	if (NOT was_malloc_unstable)
+		m_safe_assert(t_status, ft_vector_swap_remove(&ints, 0), OK, ERROR, false);
+
+	if (NOT was_malloc_unstable)
+		massert(ft_vector_get_size(ints), (size_t)998);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
