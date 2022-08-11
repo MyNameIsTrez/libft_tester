b18d5a7b632380dba8e30df6242d7b390b031974
MyNameIsTrez <sbos@student.codam.nl>
2022-08-11 15:19:30 +0200

    Remove ft_vector_get_element_size() test since I don't need it (yet)

--- tests/iterator/test_ft_vector_iterator.c
+++ tests/iterator/test_ft_vector_iterator.c
@@ -6,7 +6,7 @@
 /*   By: sbos <sbos@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/07/29 13:07:41 by sbos          #+#    #+#                 */
-/*   Updated: 2022/08/08 12:50:21 by sbos          ########   odam.nl         */
+/*   Updated: 2022/08/11 15:10:19 by sbos          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,9 +29,8 @@ Test(ft_vector_iterator)
 	m_safe_assert(void *, ints = ft_vector_new(size), ints, NULL, false);
 
 	for (int i = 0; i < (int)count; i++)
-	{
-		ft_vector_push(&ints, &i);
-	}
+		if (NOT was_malloc_unstable)
+			ft_vector_push(&ints, &i);
 
 	if (NOT was_malloc_unstable)
 	{
