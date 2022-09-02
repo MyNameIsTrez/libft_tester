524e938514015067e1d0afed42f67965349f7574
MyNameIsTrez <sbos@student.codam.nl>
2022-09-02 16:23:05 +0200

    Add test_ft_vector_from_deque

---
+++ tests/vector/test_ft_vector_from_deque.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   test_ft_vector_from_deque.c                        :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: sbos <sbos@student.codam.nl>                 +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/07/29 20:24:37 by sbos          #+#    #+#                 */
+/*   Updated: 2022/08/26 17:07:41 by sbos          ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+////////////////////////////////////////////////////////////////////////////////
+
+#include "libft_tests.h"
+
+////////////////////////////////////////////////////////////////////////////////
+
+Test(ft_vector_from_deque)
+{
+	t_deque	*deque;
+	int		v;
+
+	m_safe_assert(void *, deque = ft_deque_new_reserved(sizeof(int), 3), deque, NULL, false);
+
+	if (NOT was_malloc_unstable)
+	{
+		v = 1;
+		m_safe_assert(t_status, ft_deque_push_back(deque, &v), OK, ERROR, false);
+	}
+	if (NOT was_malloc_unstable)
+	{
+		v = 2;
+		m_safe_assert(t_status, ft_deque_push_back(deque, &v), OK, ERROR, false);
+	}
+	if (NOT was_malloc_unstable)
+	{
+		v = 3;
+		m_safe_assert(t_status, ft_deque_push_back(deque, &v), OK, ERROR, false);
+	}
+
+	int	*vector;
+
+	if (NOT was_malloc_unstable)
+	{
+		m_safe_assert(void *, vector = ft_vector_from_deque(deque), vector, NULL, false);
+	}
+
+	if (NOT was_malloc_unstable)
+	{
+		massert(vector[0], 1);
+		massert(vector[1], 2);
+		massert(vector[2], 3);
+	}
+}
+
+////////////////////////////////////////////////////////////////////////////////
