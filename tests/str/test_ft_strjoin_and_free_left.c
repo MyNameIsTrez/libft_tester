5fc7ac76c0195d90c24e28e3d2a55218a5f16efd
MyNameIsTrez <sbos@student.codam.nl>
2022-11-29 18:12:52 +0100

    Add `ft_strjoin_and_free_left()`

---
+++ tests/str/test_ft_strjoin_and_free_left.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   test_ft_strjoin_and_free_left.c                    :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: sbos <sbos@student.codam.nl>                 +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/02/17 17:11:34 by sbos          #+#    #+#                 */
+/*   Updated: 2022/06/21 10:57:41 by sbos          ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+////////////////////////////////////////////////////////////////////////////////
+
+#include "libft_tests.h"
+
+////////////////////////////////////////////////////////////////////////////////
+
+Test(ft_strjoin_and_free_left)
+{
+	char	*left = ft_strdup("lorem ipsum");
+	if (left != NULL)
+	{
+		m_safe_string_assert_free(ft_strjoin_and_free_left(left, "dolor sit amet"), "lorem ipsumdolor sit amet", true);
+	}
+}
+
+Test(ft_strjoin_and_free_left_null)
+{
+	char	*left = ft_strdup("lorem ipsum");
+	if (left != NULL)
+	{
+		m_safe_string_assert_free(ft_strjoin_and_free_left(left, NULL), "lorem ipsum", true);
+		m_safe_string_assert_free(ft_strjoin_and_free_left(NULL, "bar"), "bar", true);
+	}
+}
+
+////////////////////////////////////////////////////////////////////////////////
