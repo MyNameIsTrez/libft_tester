73cc05235e4c6eb57f3bc1b32a6d7729a8bd7467
MyNameIsTrez <sbos@student.codam.nl>
2023-02-07 14:21:29 +0100

    Add tests for ft_min and ft_max for ints and size

--- tests/nbr/test_ft_max_int.c
+++ tests/nbr/test_ft_max_int.c
@@ -1,11 +1,11 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*   test_ft_min.c                                      :+:    :+:            */
+/*   test_ft_max_int.c                                  :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: sbos <sbos@student.codam.nl>                 +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2022/02/16 17:26:34 by sbos          #+#    #+#                 */
+/*   Created: 2022/02/16 17:21:43 by sbos          #+#    #+#                 */
 /*   Updated: 2022/04/06 15:34:19 by sbos          ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
@@ -16,16 +16,16 @@
 
 ////////////////////////////////////////////////////////////////////////////////
 
-Test(ft_min)
+Test(ft_max_int)
 {
-	massert(ft_min(1, 2), 1);
-	massert(ft_min(2, 2), 2);
-	massert(ft_min(3, 2), 2);
-	massert(ft_min(-1, -2), -2);
-	massert(ft_min(-2, -2), -2);
-	massert(ft_min(-3, -2), -3);
-	massert(ft_min(0, -1), -1);
-	massert(ft_min(0, 0), 0);
+	massert(ft_max_int(1, 2), 2);
+	massert(ft_max_int(2, 2), 2);
+	massert(ft_max_int(3, 2), 3);
+	massert(ft_max_int(-1, -2), -1);
+	massert(ft_max_int(-2, -2), -2);
+	massert(ft_max_int(-3, -2), -2);
+	massert(ft_max_int(0, -1), 0);
+	massert(ft_max_int(0, 0), 0);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
